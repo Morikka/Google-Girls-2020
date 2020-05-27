262d227ef534e111e68ef931b7361a2f8a8a963f
mio <9just.k.on@gmail.com>
2020-05-27 11:19:22 +0800

    Add authentication

--- app.js
+++ app.js
@@ -1,5 +1,67 @@
 const express = require("express");
+const got = require('got');
+const jwt = require('jsonwebtoken');
+
 const app = express();
+
+let certs;
+let aud;
+
+async function certificates() {
+    if (!certs) {
+        let response = await got('https://www.gstatic.com/iap/verify/public_key');
+        certs = JSON.parse(response.body);
+    }
+
+    return certs;
+}
+
+async function getMetadata(itemName) {
+    const endpoint = 'http://metadata.google.internal';
+    const path = '/computeMetadata/v1/project/';
+    const url = endpoint + path + itemName;
+
+    let response = await got(url, {
+        headers: {'Metadata-Flavor': 'Google'},
+    });
+    return response.body;
+}
+
+async function audience() {
+    if (!aud) {
+        let project_number = await getMetadata('numeric-project-id');
+        let project_id = await getMetadata('project-id');
+        aud = '/projects/' + project_number + '/apps/' + project_id;
+    }
+    return aud;
+}
+
+async function validateAssertion(assertion) {
+    if (!assertion) {
+        return {};
+    }
+    // Decode the header to determine which certificate signed the assertion
+    const encodedHeader = assertion.split('.')[0];
+    const decodedHeader = Buffer.from(encodedHeader, 'base64').toString('utf8');
+    const header = JSON.parse(decodedHeader);
+    const keyId = header.kid;
+
+    // Fetch the current certificates and verify the signature on the assertion
+    const certs = await certificates();
+    const payload = jwt.verify(assertion, certs[keyId]);
+
+    // Check that the assertion's audience matches ours
+    const aud = await audience();
+    if (payload.aud !== aud) {
+        throw new Error('Audience mismatch. {$payload.aud} should be {$aud}.');
+    }
+
+    // Return the two relevant pieces of information
+    return {
+        email: payload.email,
+        sub: payload.sub,
+    };
+}
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
@@ -7,7 +69,18 @@ app.set('views', __dirname+'/views');
 app.set("view options", {layout: false});
 app.use('/', express.static(__dirname));
 
-app.get('/', function (req, res) {
+app.get('/', async (req, res) => {
+    const assertion = req.header('X-Goog-IAP-JWT-Assertion');
+    let email = 'None';
+    let userid = 'None';
+    try {
+        const info = await validateAssertion(assertion);
+        email = info.email;
+        // userid = info.sub;
+    } catch (error) {
+        console.log(error);
+    }
+    console.log(email,userid);
     res.render('index.html');
 });
 
