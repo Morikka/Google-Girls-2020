007c4b11cdf0b486c5865bc594f278d821ef3a5d
Mio Li <mioli@Mios-MacBook-Pro.local>
2020-06-01 11:17:24 +0800

    add database

--- Readme.md
+++ Readme.md
@@ -10,3 +10,19 @@
 
 # Google Cloud
 Webpage: https://ggirls.df.r.appspot.com
+
+
+# Settings
+
+### Authorization 
+Tutorial:
+https://cloud.google.com/nodejs/getting-started/authenticate-users
+
+### Database
+Use Mongodb Atlas
+Tutorial:
+https://cloud.google.com/community/tutorials/mongodb-atlas-appengineflex-nodejs-app
+
+### Read
++ Connection Pooling with MongoDB
+https://www.compose.com/articles/connection-pooling-with-mongodb/
--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const got = require('got');
 const jwt = require('jsonwebtoken');
-
 const app = express();
-
+const MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+const mongoose = require('mongoose');
+// The database part
+const url = "mongodb+srv://mio:Jcu3gbEBnzhd3BHL@ggirls-rw5nh.gcp.mongodb.net/test?retryWrites=true&w=majority";
+const dbName = 'ggirls';
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log("we're connected!");
+});
+// The authorization part
 let certs;
 let aud;
 
@@ -63,6 +74,18 @@ async function validateAssertion(assertion) {
     };
 }
 
+async function getUserInfo(email){
+    // need rewrite
+    // mongodb.collection("user").findOne({email:email},(err,res)=>{
+    //     if (err) throw err;
+    //     var user = res;
+    //     console.log(user);
+    //     if(res==null){
+    //         mongodb.collection("user").insertOne({email:email});
+    //     }
+    // })
+}
+
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname+'/views');
@@ -76,13 +99,15 @@ app.get('/', async (req, res) => {
     try {
         const info = await validateAssertion(assertion);
         email = info.email;
-        // userid = info.sub;
+        userid = info.sub;
     } catch (error) {
         console.log(error);
     }
     console.log(email,userid);
+    // await getUserInfo(email);
     res.render('index.html');
 });
+
 
 // Start the server
 const PORT = process.env.PORT || 8080;
