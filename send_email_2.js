e267c398965a242aac3294f051df8b00ac4c40a0
ztt0821 <35689268+ztt0821@users.noreply.github.com>
2020-07-09 16:34:16 +0800

    Update send_email_2.js

--- send_email_2.js
+++ send_email_2.js
@@ -5,7 +5,7 @@ var transporter = nodemailer.createTransport({
     service: '163',
     auth: {
         user: 'googlegirlscovid19@163.com',
-        pass: 'TIKDVKBNQOWWPMKM' //授权码,通过QQ获取
+        pass: 'TIKDVKBNQOWWPMKM' //授权码,通过163获取
     }
 });
 
@@ -21,80 +21,117 @@ var vis= json_data.vis_places;
 
 // console.log(home_case);
 // home_case
-var home_notification = "Now your home address is: "+home+".<br> We noticed these places in 500 meters near your home has cases: <br>";
-for(const item in home_case){
-    // console.log(home_case[item]);
-    const place_name = home_case[item]["mapName"];
-    var tmp = "The place "+place_name+" has cases, the cases is as follows: <br>"
-    for(const items in home_case[item]["cases"]){
-        // console.log(items)
-        tmp += "Case id is: "+home_case[item]["cases"][items]["caseID"]+", the start time is: "+home_case[item]["cases"][items]["start_date"] + " the end time is: "+home_case[item]["cases"][items]["end_date"] + "<br>";
-    }
-    home_notification += tmp;
+var home_notification = "🏠 Now your home address is: "+home+".<br>"
+if (home_case[0] === undefined){
+    home_notification += "We noticed 500 meters near your home doesn't have cases";
 }
-// console.log(home_notification);
-//work_case
-var work_notification = "Now your work address is: "+work+".<br> We noticed these places in 500 meters near your work has cases: <br>";
-for(const item in work_case){
-    console.log(work_case[item]);
-    const place_name = work_case[item]["mapName"];
-    var tmp2 = "The place "+place_name+" has cases, the cases is as follows: <br>"
-    for(const items in work_case[item]["cases"]){
-        console.log(items)
-        tmp2 += "Case id is: "+work_case[item]["cases"][items]["caseID"]+", the start time is: "+work_case[item]["cases"][items]["start_date"] + " the end time is: "+work_case[item]["cases"][items]["end_date"] + "<br>";
-    }
-    work_notification += tmp2;
-}
-console.log(work_notification);
-//fav_place
-var fav_notification = "Now your favorite place:  <br>";
-for(const item in fav){
-    console.log(fav[item]);
-    const place_name = fav[item]["mapName"];
-    if (fav[item]["flag"]){
-        var tmp2 = "The place "+place_name+" has cases, the cases is as follows: <br>"
-        for(const items in fav[item]["cases"]){
-            console.log(items)
-            tmp2 += "Case id is: "+fav[item]["cases"][items]["caseID"]+", the start time is: "+fav[item]["cases"][items]["start_date"] + " the end time is: "+fav[item]["cases"][items]["end_date"] + "<br>";
+
+else{
+    home_notification += "We noticed these places in 500 meters near your home has cases: <br>";
+    for(const item in home_case){
+        // console.log(home_case[item]);
+        const place_name = home_case[item]["mapName"];
+        var tmp = "👉" + place_name + "'s case number:"+ "😷" +String(Object.keys(home_case[item]["cases"]).length)+"<br>"
+        var i = 1;
+        for(const items in home_case[item]["cases"]){
+            // console.log(items)
+            tmp += "&emsp;"+"("+i+"):"+"Case id is: "+home_case[item]["cases"][items]["caseID"]+", the start time is: "+home_case[item]["cases"][items]["start_date"] + " the end time is: "+home_case[item]["cases"][items]["end_date"] + "<br>";
+            i += 1
         }
+        home_notification += tmp;
     }
-    else{
-        var tmp2 = "The place "+place_name+" has no case.<br>"
-    }
-    fav_notification += tmp2;
 }
+
+// work_case
+var work_notification = "💻 Now your work address is: "+work+".<br>"
+if (work_case[0] === undefined){
+    work_notification += "We noticed 500 meters near your work doesn't have cases";
+}
+
+else{
+    work_notification += "We noticed these places in 500 meters near your work has cases: <br>";
+    for(const item in work_case){
+        // console.log(home_case[item]);
+        const place_name = work_case[item]["mapName"];
+        var tmp = "👉" + place_name + "'s case number:"+ "😷" +String(Object.keys(work_case[item]["cases"]).length)+"<br>"
+        var i = 1;
+        for(const items in work_case[item]["cases"]){
+            // console.log(items)
+            tmp += "&emsp;"+"("+i+"):"+"Case id is: "+work_case[item]["cases"][items]["caseID"]+", the start time is: "+work_case[item]["cases"][items]["start_date"] + " the end time is: "+work_case[item]["cases"][items]["end_date"] + "<br>";
+            i += 1;
+        }
+        work_notification += tmp;
+    }
+}
+
+
+//fav_place
+var fav_notification = "❤️The recent information on your favourite place:<br>"
+if (fav[0] === undefined){
+    fav_notification += "We noticed 500 meters near your favourite doesn't have cases";
+}
+else{
+    fav_notification += "We noticed these places in 500 meters near your favourite place has cases: <br>";
+    for(const item in fav){
+        console.log(fav[item]);
+        const place_name = fav[item]["mapName"];
+        if (fav[item]["flag"]){
+            var tmp2 = "👉" + place_name + "'s case number:"+ "😷" +String(Object.keys(fav[item]["cases"]).length)+"<br>";
+            var i = 1;
+            for(const items in fav[item]["cases"]){
+                console.log(items)
+                tmp2 += "&emsp;"+"("+i+"):" + "Case id is: "+fav[item]["cases"][items]["caseID"]+", the start time is: "+fav[item]["cases"][items]["start_date"] + " the end time is: "+fav[item]["cases"][items]["end_date"] + "<br>";
+                i += 1;
+            }
+        }
+        else{
+            var tmp2 = "👉 "+place_name+": no case.<br>"
+        }
+        fav_notification += tmp2;
+    }
+}
+
+
 console.log(fav_notification);
 //vis_place
-var vis_notification = "Now your visit place: <br>";
-for(const item in vis){
-    console.log(vis[item]);
-    const place_name = vis[item]["mapName"];
-    const place_date = vis[item]["vis_Date"];
-    if (vis[item]["flag"]){
-        var tmp2 = "The place "+place_name+" at place_date "+ place_date + " has cases, the cases is as follows: <br>"
-        for(const items in vis[item]["cases"]){
-            console.log(items)
-            tmp2 += "Case id is: "+vis[item]["cases"][items]["caseID"]+", the start time is: "+vis[item]["cases"][items]["start_date"] + " the end time is: "+vis[item]["cases"][items]["end_date"]+"<br>";
-        }
-    }
-    else{
-        var tmp2 = "The place "+place_name+" has no case.<br>"
-
-    }
-    vis_notification += tmp2;
+var vis_notification = "🚩The recent information on your visit place:<br>"
+if (vis[0] === undefined){
+    vis_notification += "We noticed 500 meters near your favourite doesn't have cases";
 }
+else{
+    vis_notification += "We noticed these places in 500 meters near your visit place has cases: <br>";
+    for(const item in vis){
+        console.log(vis[item]);
+        const place_name = vis[item]["mapName"];
+        if (vis[item]["flag"]){
+            var i = 1;
+            var tmp2 = "👉" + place_name + "'s case number:"+ "😷" +String(Object.keys(vis[item]["cases"]).length)+"<br>";
+            for(const items in vis[item]["cases"]){
+                console.log(items)
+                tmp2 += "&emsp;"+"("+i+"):" + "Case id is: "+vis[item]["cases"][items]["caseID"]+", the start time is: "+vis[item]["cases"][items]["start_date"] + " the end time is: "+vis[item]["cases"][items]["end_date"] + "<br>";
+                i += 1;
+            }
+        }
+        else{
+            var tmp2 = "👉 "+place_name+": no case.<br>"
+        }
+        vis_notification += tmp2;
+    }
+}
+
+
 console.log(vis_notification);
 var sendHtml = `<div>
-      <div>home_case : ${home_notification}</div>
+      <div>${home_notification}</div>
       <br>
       <br>
-      <div>work_case : ${work_notification}</div>
+      <div>${work_notification}</div>
       <br>
       <br>
-      <div>fav_places : ${fav_notification}</div>
+      <div>${fav_notification}</div>
       <br>
       <br>
-      <div>vis_places : ${vis_notification}</div>
+      <div>${vis_notification}</div>
     </div>`;
 
 console.log(sendHtml);
